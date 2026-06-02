6c7a308ea1006d1ca706ae3557af8333c9c3b186
Vsevolods Mehonošins <A250652VM@rvt.lv>
2026-06-02 13:30:45 +0300

    uzdate 1.1 (new: upgrade multiplayer *1.2 // stats show // bug fixes)

--- script.js
+++ script.js
@@ -1,44 +1,60 @@
-let total = 0;
-let bonuss_clicks = 0;
+let total = 0; //total patpats
+let bonuss_clicks = 0; //bonus patpats per click
 let bonuss_clicks_cost = 10;
-let auto_click_cost = 1000;
+let multiplier = 1; //multiplier for patpats per click
 
-check_upgrade_buttons();
-document.getElementById('auto_clicks').style.display = 'none';
+document.getElementById('auto_clicks').style.display = 'none'; //hide auto click upgrade until purchased
+update_buttons_and_upgrades();
 
 function pat_button() {
-    total = total + 1 + bonuss_clicks;
+    total = total + (1 + bonuss_clicks) * multiplier;
+    total = Math.round(total * 100) / 100;
     console.log(total);
-    document.getElementById("patpat-count").textContent = total;
 
     var pat_audio = new Audio('pat_sound.wav');
     pat_audio.play();
 
-    check_upgrade_buttons();
+    update_buttons_and_upgrades();
 }
 
 function pat_button_auto() {
-    total = total + 1 + bonuss_clicks;
+    total = total + (1 + bonuss_clicks) * multiplier;
+    total = Math.round(total * 100) / 100;
     console.log(total);
-    document.getElementById("patpat-count").textContent = total;
 
     var pat_audio = new Audio('pat_sound.wav');
     pat_audio.play();
 
-    check_upgrade_buttons();
+    update_buttons_and_upgrades();
 }
 
-function check_upgrade_buttons() {
-    if (total >= bonuss_clicks_cost) {
+function update_buttons_and_upgrades() {
+    if (total >= bonuss_clicks_cost) { //double clicks upgrade
         document.getElementById("upgrade-button").disabled = false;
+        document.getElementById("bonuss_clicks").style.display = 'block';
     } else {
         document.getElementById("upgrade-button").disabled = true;
     }
-    if (total >= auto_click_cost) {
+    if (total >= 1000) { //auto click upgrade
         document.getElementById("auto_click_button").disabled = false;
+        document.getElementById("auto_clicks_count").disabled = false;
+        document.getElementById('auto_clicks_count').style.display = 'block';
     } else {
         document.getElementById("auto_click_button").disabled = true;
+        document.getElementById("auto_clicks_count").disabled = true;
     }
+    if (total >= 400) { //multiplayer upgrade
+        document.getElementById("upgrade_plus_1_2_clicks").disabled = false;
+        document.getElementById("multiplier").style.display = 'block';
+    } else {
+        document.getElementById("upgrade_plus_1_2_clicks").disabled = true;
+    }
+
+    //Updating upgeades and total pats
+    document.getElementById("patpat_count").textContent = Math.round(total);
+    document.getElementById("bonuss_clicks").textContent = bonuss_clicks;
+    document.getElementById("multiplier").textContent = multiplier;
+
 }
 
 function upgrade_double_clicks() {
@@ -49,27 +65,39 @@ function upgrade_double_clicks() {
     else {    
         total = total - bonuss_clicks_cost;
         bonuss_clicks_cost = bonuss_clicks_cost * 2;
-        document.getElementById("patpat-count").textContent = total;
         document.getElementById("upgrade-cost").textContent = bonuss_clicks_cost;
         bonuss_clicks = bonuss_clicks + 1;
         console.log(bonuss_clicks);
-        check_upgrade_buttons();
+        update_buttons_and_upgrades();
     }
 }
 
-function auto_click() {
-    if (total < auto_click_cost) {
+function upgrade_plus_1_2_clicks() {
+    if (total < 400) {
         alert("Not enough patpats to upgrade!");
         return;
     }
     else {
-        total = total - auto_click_cost;
-        document.getElementById("patpat-count").textContent = total;
-        document.getElementById("auto-click-cost").textContent = auto_click_cost;
+        total = total - 400;
+        multiplier = 1.2;
+        console.log(multiplier);
+        document.getElementById('upgrade_plus_1_2_clicks').style.display = 'none';
+        update_buttons_and_upgrades();
+    }
+}
+
+function auto_click() {
+    if (total < 1000) {
+        alert("Not enough patpats to upgrade!");
+        return;
+    }
+    else {
+        total = total - 1000;
+        document.getElementById("auto-click-cost").textContent = 1000;
         document.getElementById('auto_clicks').style.display = 'block';
         document.getElementById('auto_click_button').style.display = 'none';
         interval();
-        check_upgrade_buttons();
+        update_buttons_and_upgrades();
     }
 }
 
